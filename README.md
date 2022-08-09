a1be272d6e58f5a1a1691979fc2bef2ac57f9577
Michal Grecner <grecnermichal@gmail.com>
2022-08-09 23:03:03 +0200

    added working slider selector for grid size

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+add a retro font
+add  a slide selector for grid size
--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-let squares = 64; //squares per line
-let pixels = 600; //canvas width/height
 
-let oneSquare = pixels/squares
-console.log("size of one square: " +oneSquare)
 
-makeCanvas(oneSquare)
+function makeCanvas(squares = 50){
+    let pixels = 600;
+    let oneSquare = pixels/squares
 
-function makeCanvas(oneSquare){
     let canvas = document.querySelector(".canvas");
     let square = document.createElement("div");
     for(let x=0; x <Math.floor(squares); x++){
         for(let y=0; y<Math.floor(squares); y++){
             let square = document.createElement("div");
+            square.setAttribute("class","divSquare");
             square.style.border = "1px solid black";
             square.style.width = `${oneSquare}px`
             square.style.height =`${oneSquare}px`
             square.style.display = "flex"
             square.style.boxSizing = "border-box"
-
             canvas.appendChild(square)
-            
-           
-            
-
         }
-
     }
+}
+ getSize()
+
+function getSize(){
+    let slider = document.querySelector("#slider");
+    let sliderValue = 50;
+    let sliderValueDisplay = document.querySelector(".sliderValue")
+    slider.addEventListener("change", function(e){
+        removeCanvas()
+        sliderValue = e.target.value;
+        sliderValueDisplay.innerHTML=sliderValue
+        makeCanvas(sliderValue)
+    })
+}
+
+function removeCanvas(){
+    let divs = document.querySelectorAll(".divSquare")
+    divs.forEach(function(oneDiv){
+        oneDiv.remove()
+    })
 }
