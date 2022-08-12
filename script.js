2d12ff7c40cdc6a039f39d21a6b1f02e8d4503f0
Michal Grecner <grecnermichal@gmail.com>
2022-08-12 23:27:36 +0200

    added change pen and canvas color change

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 
-function makeCanvas(squares = 50){
-    let sliderValueDefault = document.querySelector(".sliderValue")
-    sliderValueDefault.innerHTML = `Grid size: ${squares} x ${squares}`
+function makeCanvas(squares = 50, color = "blue"){
+    let sliderValueDefault = document.querySelector(".sliderValue");
+    sliderValueDefault.innerHTML = `Grid size: ${squares} x ${squares}`;
     let pixels = 600;
-    let oneSquare = pixels/squares
+    let oneSquare = pixels/squares;
     let canvas = document.querySelector(".canvas");
-    let square = document.createElement("div");
-    
-
+    document.querySelector("#colorPickerCanvas").addEventListener("input", function(e){
+         canvasColor = e.target.value;
+    })
     for(let x=0; x <Math.floor(squares); x++){
         for(let y=0; y<Math.floor(squares); y++){
             let square = document.createElement("div");
@@ -18,23 +18,42 @@ function makeCanvas(squares = 50){
             square.style.height =`${oneSquare}px`
             square.style.display = "flex"
             square.style.boxSizing = "border-box"
+            square.style.backgroundColor = `${color}`
             canvas.appendChild(square)
         }
     }
+    
+    draw(globalPenColor)
+    
+    
 }
 
 
-function getSize(){
+function updateCanvas(){
     let slider = document.querySelector("#slider");
     let sliderValue = 50;
     let sliderValueDisplay = document.querySelector(".sliderValue")
     slider.addEventListener("change", function(e){
-        removeCanvas()
         sliderValue = e.target.value;
-        sliderValueDisplay.innerHTML=`Grid size: ${e} x ${e}`
-        makeCanvas(sliderValue)
+        sliderValueDisplay.innerHTML=`Grid size: ${sliderValue} x ${sliderValue}`
+        
     })
+    let canvasColor = "blue"
+    document.querySelector("#colorPickerCanvas").addEventListener("input", function(e){
+        canvasColor = e.target.value;
+   })
+
+   let btn = document.querySelector("#btnUpdate").addEventListener("click", function(){
+    removeCanvas()
+    makeCanvas(sliderValue, canvasColor)
+   })
+    
+
+
 }
+
+
+
 
 function removeCanvas(){
     let divs = document.querySelectorAll(".divSquare")
@@ -43,5 +62,27 @@ function removeCanvas(){
     })
 } 
 
+var globalPenColor = "#ff0000"
+
+function draw(color="#ff0000"){
+    document.querySelector("#colorPickerPen").addEventListener("input", function(e){
+        color= e.target.value;
+        globalPenColor = color
+    })
+    let hoverSquare = document.querySelectorAll(".divSquare");
+    hoverSquare.forEach(function(oneSquareHover){
+        oneSquareHover.addEventListener("mousemove", function(e){
+            if(e.buttons){
+                e.target.style.backgroundColor = `${color}`
+            }
+        })
+    })
+}
+
+
+
+
+
 makeCanvas()
-getSize()
+updateCanvas()
+draw()
